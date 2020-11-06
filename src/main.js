f2a274428ccc4177b8a04a1c988f2c6fe4adda52
lzzhuang <2078784331@qq.com>
2020-11-06 15:43:11 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import '@/icons';
+import '@/styles/index.scss' // global css
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
